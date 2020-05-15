9a25e92d7095494f3c038d9669ae33df270c2e04
Mark Torrico <marcojtorrico@gmail.com>
2020-05-14 22:42:07 -0400

    time1602

--- main/almacigo.py
+++ main/almacigo.py
@@ -27,15 +27,14 @@ def process():
         blink_blue_led()                              # BBL
         date = time_date.MyTimeDate()      # read Time&Date
         dt = date.readTimeDate()
-        print(dt[0],":",dt[1],"/",dt[2],"/",dt[3],"/",dt[4])
+        #print(dt[0],":",dt[1]," ",dt[2],".",dt[3],".",dt[4])
         if dt[0] == 21 and dt[1] == 0:  # refresh Time&Date
             date = time_date.MyTimeDate()
             dt = date.readTimeDate()
             newFirmware()   # CHECK/DOWNLOAD/INSTALL/REBOOT
         if dt[0] == 4 and dt[1] == 30:
             rt = rutina.Riego()
-        LCD = mp_i2c_lcd1602.LCD1602(i2c)
-        LCD.puts("I2C LCD1602")
+        print_date_time()               # LCD1602 date&time
 
 # ----------------------------------------------------------
 
@@ -52,3 +51,23 @@ def newFirmware():
     ota_updater = ota_updater.OTAUpdater('https://github.com/mastercba/almacigo')
     ota_updater.download_and_install_update_if_available('TORRIMORA', 'santino989')
     ota_updater.check_for_update_to_install_during_next_reboot()   
+
+# LCD1602 date&time
+def print_date_time():
+    date = time_date.MyTimeDate()      # read Time&Date
+    dt = date.readTimeDate()
+    LCD = mp_i2c_lcd1602.LCD1602(i2c)  # print LCD1602
+    if dt[1] < 10:
+        LCD.puts(dt[0], 0, 0)
+        LCD.puts(":", 2, 0)
+        LCD.puts("0", 3, 0)
+        LCD.puts(dt[1], 4, 0)
+    elif dt[0] < 10:
+        LCD.puts("0", 0, 0)
+        LCD.puts(dt[0], 1, 0)
+        LCD.puts(":", 2, 0)
+        LCD.puts(dt[1], 3, 0)
+    else:
+        LCD.puts(dt[0], 0, 0)
+        LCD.puts(":", 2, 0)
+        LCD.puts(dt[1], 3, 0)
